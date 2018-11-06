dddc510bac5c706ef48a9806d79ec742b95f3601
SANJAY K SAI <shachsan@hotmail.com>
2018-11-06 14:39:48 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,7 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
-  WHERE id = (SELECT series_id FROM (
+   WHERE id = (SELECT series_id FROM (
   select series_id, species,  COUNT(species) from characters WHERE species = 'human' GROUP BY series_id ORDER BY COUNT(species) DESC LIMIT 1));"
 end
 
